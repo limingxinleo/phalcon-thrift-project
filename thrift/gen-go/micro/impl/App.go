ae25ae02a6e5ea42b099021fda35269bd4b5d04b
李铭昕 <715557344@qq.com>
2017-09-03 12:39:18 +0800

    * 增加日志依赖

--- thrift/gen-go/micro/impl/App.go
+++ thrift/gen-go/micro/impl/App.go
@@ -1,9 +1,16 @@
 package impl
 
+import log "github.com/sirupsen/logrus"
+
+func init() {
+
+}
+
 type App struct {
 }
 
 func (this *App) Version() (r string, err error) {
+	log.Info("App::version");
 	r = "1.10.8"
 	return
 }
