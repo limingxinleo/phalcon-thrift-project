2e20d9a196d0040e2697f539fe7315e9e54f509f
李铭昕 <715557344@qq.com>
2017-09-16 12:54:19 +0800

    * 增加php 版本Services服务

---
+++ app/thrift/Services/AppHandler.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | AppHandler.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace App\Thrift\Services;
+
+use MicroService\AppIf;
+
+class AppHandler extends Handler implements AppIf
+{
+    public function version()
+    {
+        return $this->config->version;
+    }
+
+}
---
+++ app/thrift/Services/Handler.php
@@ -0,0 +1,16 @@
+<?php
+// +----------------------------------------------------------------------
+// | Base.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace App\Thrift\Services;
+
+use Phalcon\Di\Injectable;
+
+class Handler extends Injectable
+{
+
+}
